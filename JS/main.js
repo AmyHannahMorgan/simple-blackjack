0f1df6a4f05f51473856ab48e309c39ff873aa1b
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-07-01 21:30:48 +0100

    added event dispatch on click of play button

--- JS/main.js
+++ JS/main.js
@@ -5,9 +5,12 @@ const rulesModal = document.querySelector('#rulesModal');
 const startMenu = document.querySelector(".startMenu");
 const gameScreen = document.querySelector(".gameScreen");
 
+const startGame = new CustomEvent('gameStart', {});
+
 playButton.addEventListener('click', _ => {
     startMenu.style.display = "none";
     gameScreen.style.display = "block";
+    window.dispatchEvent(startGame);
 });
 
 rulesButton.addEventListener('click', _ =>{
