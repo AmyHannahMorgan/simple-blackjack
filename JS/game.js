bcb048c5906c4d23bbdabf94c29bd93df6cf7493
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-07-01 21:54:47 +0100

    added inital variables

--- JS/game.js
+++ JS/game.js
@@ -1,5 +1,21 @@
+const tManifestURL = "JS/Data/texture-manifest.json";
+let textures;
 
+let manifestReq = new XMLHttpRequest();
+manifestReq.open('GET', tManifestURL);
+manifestReq.addEventListener('load', function() {
+    textures = JSON.parse(this.responseText);
+});
+manifestReq.send();
+
+const ggameScreen = document.querySelector('.gameScreen');
+
+const dealerHand = ggameScreen.querySelector('#dealerHand');
+const playerHand = ggameScreen.querySelector('#playerHand');
+
+const dealerScore = ggameScreen.querySelector('#dealerScore');
+const playerScore = ggameScreen.querySelector('#playerScore');
 
 window.addEventListener('gameStart', _ => {
-    
+    console.log('event fired');
 });
