4aac22c6d02fcb9c1af4cd8e1274e818ce5b39f2
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-07-02 06:32:58 +0100

    created card and deck clases

---
+++ JS/Modules/cards.js
@@ -0,0 +1,78 @@
+export class Card {
+    constructor(suit, number, name, flipped) {
+        this.suit = suit;
+        this.number = number;
+        this.name = name;
+        this.flipped = flipped;
+        this.element = null;
+    }
+    
+    link(element) {
+        this.element = element;
+    }
+
+    flip() {
+        if(this.element !== null) {
+            if(this.flipped) {
+                this.flipped = false;
+                this.element.classList.remove('flip');
+            }
+            else {
+                this.flipped = true;
+                this.element.classList.add('flip');
+            }
+        }
+        else throw "there is no element to flip";
+    }
+
+    get details() {
+        let details = {
+            "suit": this.suit,
+            "name": this.name
+        }
+
+        return details;
+    }
+}
+
+export class deck {
+    constructor(deckCount) {
+        let cardDict = {
+            1: "ace",
+            11: "jack",
+            12: "queen",
+            13: "king"
+        }
+        let suits = ['spades', 'hearts', 'clubs', 'diamonds'];
+        this.cards = [];
+        for(let i = 0; i < deckCount; i++) {
+            for(let s = 0; s < suits.length; s++) {
+                for(let c = 1; c < 14; c++) {
+                    let name;
+                    if(Object.keys(cardDict).includes(c)) {
+                        name = cardDict[c];
+                    }
+                    else {
+                        name = `${c}`;
+                    }
+                    let card = new Card(suits[s], c, name, false);
+                    this.cards.push(card);
+                }
+            }
+        }
+    }
+
+    shuffle() {
+        let shuffled = [];
+        for(let i = 0; this.cards.length > 0; i++) {
+            let index = rng(0, this.cards.length);
+            shuffled.push(this.cards.splice(index, 1));
+        }
+        this.cards = shuffled;
+        return this.cards;
+    }
+}
+
+function rng(min, max) {
+    return Math.random() * (max - min) + min;
+}
